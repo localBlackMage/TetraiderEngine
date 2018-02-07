39501327f94887886b86d591ad38a8d1bdd3d7f2
Acrylica <holdenprofit@gmail.com>
2018-02-06 16:32:13 -0800

    Removed some debug stuff from my MM work.

--- TetraiderEngine/Source/Subscriber.cpp
+++ TetraiderEngine/Source/Subscriber.cpp
@@ -1,12 +1,6 @@
 #include "Subscriber.h"
 #include "TetraiderAPI.h"
 #include "SubscriberTracker.h"
-#include <unordered_map>
-#include <string>
-
-using namespace std;
-static unordered_map<string, int> pointerCount;
-
 
 Subscriber::Subscriber()
 {
@@ -47,33 +41,19 @@ void Subscriber::SubscribeToEvent(EventType eventType)
 
 void* Subscriber::operator new(std::size_t size)
 {
-	void* val = TETRA_MEMORY.Alloc(size);
-
-	char str[40];
-	sprintf_s(str, "%p\n", val);
-	++pointerCount[str];
-
-	if (pointerCount[str] > 1) {
-		printf("Duplicate address returned!");
-	}
-	return val;
+	return TETRA_MEMORY.Alloc(size);
 }
 
 void Subscriber::operator delete(void *ptr)
 {
-	char str[40];
-	sprintf_s(str, "%p\n", ptr);
-	--pointerCount[str];
 	TETRA_MEMORY.Free(ptr);
 }
 void* Subscriber::operator new[](std::size_t size)
 {
-	printf("Allocating array.\n");
 	return TETRA_MEMORY.Alloc(size);
 }
 
 void Subscriber::operator delete[](void* arrayPtr)
 {
-	printf("Deallocating array.\n");
 	TETRA_MEMORY.Free(arrayPtr);
 }
--- TetraiderEngine/Source/main.cpp
+++ TetraiderEngine/Source/main.cpp
@@ -4,7 +4,6 @@
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd)
 {
 	Tetraider::Initialize("../TetraiderEngine/Assets/Metadata/Config.json");
-	printf("============= Program Start ============\n\n");
 	Tetraider::StartGameLoop();
 
 	return 0;
