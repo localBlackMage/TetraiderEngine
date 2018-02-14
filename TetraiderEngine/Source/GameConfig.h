b4965832c1dc7ce349852a6123d1dd786d8542d8
Acrylica <holdenprofit@gmail.com>
2018-02-14 11:48:28 -0800

    Got rid of the 'magic numbers' associated with the room dimensions in favor of a cell size + room size (in cells).

--- TetraiderEngine/Source/GameConfig.h
+++ TetraiderEngine/Source/GameConfig.h
@@ -4,6 +4,7 @@
 #define GAME_CONFIG_H
 
 #include <string>
+#include <utility>
 #include "JsonReader.h"
 
 using json = nlohmann::json;
@@ -17,6 +18,9 @@ private:
 	std::string m_sfxDir;
 	std::string m_shadersDir;
 	std::string m_roomFilesDir;
+
+	unsigned short m_cellWidth, m_cellHeight;	// Cell dimensions in pixels (ex. 96x96)
+	unsigned short m_roomWidth, m_roomHeight;	// Room dimensions in cells (ex. 12x8)
 
 public:
 	GameConfig();
@@ -32,6 +36,11 @@ public:
 	std::string SFXDir() const { return m_sfxDir; }
 	std::string ShadersDir() const { return m_shadersDir; }
 	std::string RoomFilesDir() const { return m_roomFilesDir; }
+
+	unsigned short CellWidth() const { return m_cellWidth; }
+	unsigned short CellHeight() const { return m_cellHeight; }
+	unsigned short RoomWidth() const { return m_roomWidth; }
+	unsigned short RoomHeight() const { return m_roomHeight; }
 };
 
 #endif
