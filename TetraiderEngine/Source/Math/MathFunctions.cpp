78ee5c43292fcc9c970110188223ce41a2f72aec
hyoyup chung <hyoyup.c@digipen.edu>
2018-02-23 15:06:31 -0800

    random functions in MathFunctions.h/cpp

--- TetraiderEngine/Source/Math/MathFunctions.cpp
+++ TetraiderEngine/Source/Math/MathFunctions.cpp
@@ -1,5 +1,6 @@
 #include "MathFunctions.h"
 #include <cmath>
+#include <stdlib.h>
 
 Vector3D Lerp(const Vector3D& vectorA, const Vector3D& vectorB, float t) {
 	return Vector3D(Lerp(vectorA.x, vectorB.x, t), Lerp(vectorA.y, vectorB.y, t), Lerp(vectorA.z, vectorB.z, t), Lerp(vectorA.w, vectorB.w, t));
@@ -24,4 +25,14 @@ float SinWave(float amplitude, float frequency, float phaseShift, float time) {
 
 float CosWave(float amplitude, float frequency, float phaseShift, float time) {
 	return amplitude*cosf(frequency*time + phaseShift);
+}
+
+float RandomFloat(float min, float max) {
+	srand(TETRA_FRAMERATE.GetElapsedTime());
+	return static_cast<float>(rand()) / max + min;
+}
+
+int RandomInt(int min, int max) {
+	srand(TETRA_FRAMERATE.GetElapsedTime());
+	return rand() / max + min;
 }
--- TetraiderEngine/Source/Math/MathFunctions.h
+++ TetraiderEngine/Source/Math/MathFunctions.h
@@ -3,11 +3,15 @@
 
 #include "Vector3D.h"
 #include "Vector2D.h"
+#include "..\TetraiderAPI.h"
 
 Vector3D Lerp(const Vector3D& vectorA, const Vector3D& vectorB, float t);
 Vector2D Lerp(const Vector2D& vectorA, const Vector2D& vectorB, float t);
 float Lerp(float valueA, float valueB, float t);
 float SinWave(float amplitude, float frequency, float phaseShift, float time);
 float CosWave(float amplitude, float frequency, float phaseShift, float time);
+float RandomFloat(float min, float max);
+int RandomInt(int min, int max);
+
 
 #endif
