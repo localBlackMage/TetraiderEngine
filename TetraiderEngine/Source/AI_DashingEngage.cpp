59a3711a4e8ca7d339995551ddad7496a78a33ad
hyoyup chung <hyoyup.c@digipen.edu>
2018-02-26 17:38:17 -0800

    ai dashing

--- TetraiderEngine/Source/AI_DashingEngage.cpp
+++ TetraiderEngine/Source/AI_DashingEngage.cpp
@@ -12,12 +12,12 @@ AI_DashingEngage::~AI_DashingEngage() {
 void AI_DashingEngage::OnEnter() {
 	sinceEngage = 0.0f;
 	engageTimer = 3;
+	//play pre-dashing animation!
 }
 
 void AI_DashingEngage::OnUpdate(float dt) {
 	// always face player on engage
 	pAgent->LookAtPlayer();
-	// if this engage has gone too long, just go attack!
 	if (sinceEngage > engageTimer) {
 		pAgent->ChangeState(NPC_ATTACK);
 		return;
