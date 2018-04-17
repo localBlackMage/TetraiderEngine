e28ba287f8c50587968569da51cf90d83135ca54
Moodie <moodie.ghaddar@digipen.edu.lb>
2018-04-17 13:58:20 -0700

    Fixed mouse bug and full screen pause bug

--- TetraiderEngine/Source/Camera.cpp
+++ TetraiderEngine/Source/Camera.cpp
@@ -115,16 +115,12 @@ float Camera::GetAspect() const
 
 Vector3D Camera::TransformPointToScreenSpace(const Vector3D& worldCoordinates) {
 	// TODO: talk to moodie
-	Matrix4x4 viewPerspectiveMatrix = Matrix4x4::Orthographic(
-		float(m_screenWidth),
-		float(m_screenHeight),
-		0.1f) * 
-		GetViewMatrix(); 
+	Matrix4x4 viewPerspectiveMatrix = GetCameraMatrix() * GetViewMatrix(); 
 	
 	// Transform point to clipping coordinates
 	Vector3D result = viewPerspectiveMatrix*worldCoordinates;
-	result.x = ((result.x / result.w) + 1.f) / 2.0f * m_screenWidth;
-	result.y = (1 - (result.y / result.w)) / 2.0f * m_screenHeight;
+	result.x = ((result.x / result.w) + 1.f) / 2.0f * m_screenWidth*m_zoomLevels[m_currentZoomIndex];
+	result.y = (1 - (result.y / result.w)) / 2.0f * m_screenHeight * m_zoomLevels[m_currentZoomIndex];
 	result.z = 0;
 	result.w = 1;
 
