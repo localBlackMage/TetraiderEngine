a7a3999055178323a66752cc2e2669f2d4b35f16
Moodie <moodie.ghaddar@digipen.edu.lb>
2018-04-19 13:26:51 -0700

    Added more copyright

--- TetraiderEngine/Source/Button.cpp
+++ TetraiderEngine/Source/Button.cpp
@@ -2,7 +2,7 @@
 Copyright (C) 2018 DigiPen Institute of Technology.
 Reproduction or disclosure of this file or its contents without the prior
 written consent of DigiPen Institute of Technology is prohibited.
-Author: <Holden Profit>
+Author: <Sujay Shah>
 - End Header --------------------------------------------------------*/
 
 #include <Stdafx.h>
